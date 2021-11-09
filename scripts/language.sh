d2d75b392c216d76007413897df61edb7967483c
Zlatomir Haralambov <z.haralambov@gmail.com>
2021-11-09 18:51:31 +0200

    Adds script for checking keyboard layout and signal dwmblocks

---
+++ scripts/language.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CURRENT_LANGUAGE=$(setxkbmap -query | grep -i layout | awk '{print $2}' | cut -d\, -f1)
+if [[ $CURRENT_LANGUAGE == "bg" ]]
+then
+    $(setxkbmap -layout us,bg -variant ,phonetic)
+else
+    $(setxkbmap -layout bg,us -variant phonetic,)
+fi
+# update the block value
+# 34 + the signal number
+kill -44 $(pidof dwmblocks)
