4b93640d62b989865a2ae15de3392900683747b0
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-01-26 19:50:35 +0200

    Updates volume script to use pulsemixer

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-COMMAND=$1
-if [ $COMMAND == "--inc" ]; then
-    $(pactl set-sink-volume 0 +5%)
-    $(pactl set-sink-mute 0 0)
-elif [ $COMMAND == "--dec" ]; then
-    $(pactl set-sink-volume 0 -5%)
-elif [ $COMMAND == "--mute" ]; then
-    $(pactl set-sink-volume 0 0%)
-    $(pactl set-sink-mute 0 toggle)
+ARGUMENT=$1
+if [ "$ARGUMENT" = "--inc" ]; then
+    pulsemixer --change-volume +5
+elif [ "$ARGUMENT" = "--dec" ]; then
+    pulsemixer --change-volume -5
+elif [ "$ARGUMENT" = "--mute" ]; then
+    pulsemixer --toggle-mute
+else
+    MUTE=$(pulsemixer --get-mute)
+    if [ $MUTE -eq 0 ]; then
+        VOLUME_LEVEL=$(pulsemixer --get-volume | awk '{print $1}')
+        echo -e " $VOLUME_LEVEL"
+    else
+        echo -e ""
+    fi
 fi
 
+# Needed for dwmblocks
 # update the block value
 # 34 + the signal number
-kill -46 $(pidof dwmblocks)
+# kill -46 $(pidof dwmblocks)
