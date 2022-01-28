b5f39168e84b400284da8f4df1a8f66678935b02
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-01-28 21:46:26 +0200

    Adds creating a cache file for volume, since the command is slow and also removes dwmblock signaling

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+CACHE_FILE=~/.cache/volume
+
 ARGUMENT=$1
 if [ "$ARGUMENT" = "--inc" ]; then
     pulsemixer --change-volume +5
+    rm $CACHE_FILE > /dev/null
 elif [ "$ARGUMENT" = "--dec" ]; then
     pulsemixer --change-volume -5
+    rm $CACHE_FILE > /dev/null
 elif [ "$ARGUMENT" = "--mute" ]; then
     pulsemixer --toggle-mute
+    rm $CACHE_FILE > /dev/null
 else
-    MUTE=$(pulsemixer --get-mute)
-    if [ $MUTE -eq 0 ]; then
-        VOLUME_LEVEL=$(pulsemixer --get-volume | awk '{print $1}')
-        echo -e " $VOLUME_LEVEL"
-    else
-        echo -e ""
+    if [ ! -f "$CACHE_FILE" ]; then
+        MUTE=$(pulsemixer --get-mute)
+        if [ $MUTE -eq 0 ]; then
+            VOLUME_LEVEL=$(pulsemixer --get-volume | awk '{print $1}')
+            echo -e " $VOLUME_LEVEL" > $CACHE_FILE
+        else
+            echo -e " muted" > $CACHE_FILE
+        fi
     fi
+    cat $CACHE_FILE
 fi
 
-# Needed for dwmblocks
-# update the block value
-# 34 + the signal number
-# kill -46 $(pidof dwmblocks)
