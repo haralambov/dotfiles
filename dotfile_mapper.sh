5d8132a2fece40d4d7ba68d69788ed521c034dfb
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-04-21 20:33:46 +0300

    Comments out creating of unused symlinks

--- dotfile_mapper.sh
+++ dotfile_mapper.sh
@@ -16,40 +16,40 @@ fi
 
 ln -s "$PWD/.bashrc" "$HOME/.bashrc"
 
-# map compton config
-if [ -f "$HOME/.config/compton.conf" ]; then
-    rm "$HOME/.config/compton.conf"
-fi
-
-ln -s "$PWD/config/compton/compton.conf" "$HOME/.config/compton.conf"
-
-# maps touchpad config, that enables tap to click
-if [ -f "/etc/X11/xorg.conf.d/40-libinput.conf" ]; then
-    sudo rm "/etc/X11/xorg.conf.d/40-libinput.conf"
-fi
-
-sudo ln -s "$PWD/config/touchpad/40-libinput.conf" "/etc/X11/xorg.conf.d/40-libinput.conf"
-
-if [ -f "/usr/local/bin/locker" ]; then
-    sudo rm "/usr/local/bin/locker"
-fi
-
-sudo ln -s "$PWD/scripts/locker" "/usr/local/bin/locker"
-
-if [ -f "/usr/local/bin/passmenu" ]; then
-    sudo rm "/usr/local/bin/passmenu"
-fi
-
-sudo ln -s "$PWD/scripts/passmenu" "/usr/local/bin/passmenu"
-
-# maps newsboat RSS reader config
-if [ ! -d "$HOME/.newsboat" ]; then
-    mkdir "$HOME/.newsboat"
-else
-    rm "$HOME/.newsboat/urls"
-    rm "$HOME/.newsboat/config"
-fi
-
-ln -s "$PWD/config/newsboat/config" "$HOME/.newsboat/config"
-ln -s "$PWD/config/newsboat/urls" "$HOME/.newsboat/urls"
-
+## map compton config
+#if [ -f "$HOME/.config/compton.conf" ]; then
+#    rm "$HOME/.config/compton.conf"
+#fi
+#
+#ln -s "$PWD/config/compton/compton.conf" "$HOME/.config/compton.conf"
+#
+## maps touchpad config, that enables tap to click
+#if [ -f "/etc/X11/xorg.conf.d/40-libinput.conf" ]; then
+#    sudo rm "/etc/X11/xorg.conf.d/40-libinput.conf"
+#fi
+#
+#sudo ln -s "$PWD/config/touchpad/40-libinput.conf" "/etc/X11/xorg.conf.d/40-libinput.conf"
+#
+#if [ -f "/usr/local/bin/locker" ]; then
+#    sudo rm "/usr/local/bin/locker"
+#fi
+#
+#sudo ln -s "$PWD/scripts/locker" "/usr/local/bin/locker"
+#
+#if [ -f "/usr/local/bin/passmenu" ]; then
+#    sudo rm "/usr/local/bin/passmenu"
+#fi
+#
+#sudo ln -s "$PWD/scripts/passmenu" "/usr/local/bin/passmenu"
+#
+## maps newsboat RSS reader config
+#if [ ! -d "$HOME/.newsboat" ]; then
+#    mkdir "$HOME/.newsboat"
+#else
+#    rm "$HOME/.newsboat/urls"
+#    rm "$HOME/.newsboat/config"
+#fi
+#
+#ln -s "$PWD/config/newsboat/config" "$HOME/.newsboat/config"
+#ln -s "$PWD/config/newsboat/urls" "$HOME/.newsboat/urls"
+#
