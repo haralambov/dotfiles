07be00a607063285c35ff870c31f6235b7b64a94
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-08-20 11:05:59 +0300

    Fixes lsp tab completion

--- nvim/lua/plugins_configs/cmp.lua
+++ nvim/lua/plugins_configs/cmp.lua
@@ -1,5 +1,11 @@
 -- Setup nvim-cmp.
 local cmp = require'cmp'
+local luasnip = require'luasnip'
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 cmp.setup({
     snippet = {
@@ -41,10 +47,7 @@ cmp.setup({
     }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
-        { name = 'vsnip' }, -- For vsnip users.
-        -- { name = 'luasnip' }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
-        -- { name = 'snippy' }, -- For snippy users.
+        { name = 'luasnip' }, -- For luasnip users.
     }, {
         { name = 'buffer' },
     })
