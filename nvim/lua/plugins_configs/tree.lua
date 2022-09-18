2fa85b7726c423c99f9e7168d26a0e2d961e61f5
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-09-18 09:26:40 +0300

    Adds nvim-tree config

--- nvim/lua/plugins_configs/tree.lua
+++ nvim/lua/plugins_configs/tree.lua
@@ -1,3 +1,14 @@
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+    open_on_setup = true,
+    update_cwd = true,
+    update_focused_file = {
+        enable = true,
+        update_cwd = true,
+    },
+    view = {
+        side = "left",
+        width = 35
+    }
+})
 
 vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true })
