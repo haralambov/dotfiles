13528bbd4a114fd62868ebeef3a0dfcd2c7f6aec
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-09-29 18:25:46 +0300

    Updates the config for nvim-tree

--- nvim/lua/plugins_configs/tree.lua
+++ nvim/lua/plugins_configs/tree.lua
@@ -1,9 +1,7 @@
 require("nvim-tree").setup({
     open_on_setup = true,
-    update_cwd = true,
     update_focused_file = {
-        enable = true,
-        update_cwd = true,
+        enable = true
     },
     view = {
         side = "left",
