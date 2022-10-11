57602493140ff7708a09de4283b17ff1aee6c2ed
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-10-11 20:00:25 +0300

    Removes unused config symlinks

--- dotfile_mapper.sh
+++ dotfile_mapper.sh
@@ -25,33 +25,16 @@ ln -s "$PWD/.bashrc" "$HOME/.bashrc"
 #
 #ln -s "$PWD/config/compton/compton.conf" "$HOME/.config/compton.conf"
 #
-## maps touchpad config, that enables tap to click
-#if [ -f "/etc/X11/xorg.conf.d/40-libinput.conf" ]; then
-#    sudo rm "/etc/X11/xorg.conf.d/40-libinput.conf"
-#fi
-#
-#sudo ln -s "$PWD/config/touchpad/40-libinput.conf" "/etc/X11/xorg.conf.d/40-libinput.conf"
-#
-#if [ -f "/usr/local/bin/locker" ]; then
-#    sudo rm "/usr/local/bin/locker"
-#fi
-#
-#sudo ln -s "$PWD/scripts/locker" "/usr/local/bin/locker"
-#
-#if [ -f "/usr/local/bin/passmenu" ]; then
-#    sudo rm "/usr/local/bin/passmenu"
-#fi
-#
-#sudo ln -s "$PWD/scripts/passmenu" "/usr/local/bin/passmenu"
-#
-## maps newsboat RSS reader config
-#if [ ! -d "$HOME/.newsboat" ]; then
-#    mkdir "$HOME/.newsboat"
-#else
-#    rm "$HOME/.newsboat/urls"
-#    rm "$HOME/.newsboat/config"
-#fi
-#
-#ln -s "$PWD/config/newsboat/config" "$HOME/.newsboat/config"
-#ln -s "$PWD/config/newsboat/urls" "$HOME/.newsboat/urls"
-#
+# maps touchpad config, that enables tap to click
+if [ -f "/etc/X11/xorg.conf.d/40-libinput.conf" ]; then
+    sudo rm "/etc/X11/xorg.conf.d/40-libinput.conf"
+fi
+
+sudo ln -s "$PWD/config/touchpad/40-libinput.conf" "/etc/X11/xorg.conf.d/40-libinput.conf"
+
+if [ -f "/usr/local/bin/locker" ]; then
+    sudo rm "/usr/local/bin/locker"
+fi
+
+sudo ln -s "$PWD/scripts/locker" "/usr/local/bin/locker"
+
