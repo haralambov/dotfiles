2247f2602f3bb03cd0ff299a5bfa442bc96c7649
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-10-14 20:57:40 +0300

    Adds shortcut for resuming telescope

--- nvim/lua/plugins_configs/telescope.lua
+++ nvim/lua/plugins_configs/telescope.lua
@@ -2,4 +2,5 @@ vim.api.nvim_set_keymap('n', '<LEADER>ff', ':Telescope find_files hidden=true<CR
 vim.api.nvim_set_keymap('n', '<LEADER>fg', ':Telescope live_grep<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<LEADER>fb', ':Telescope buffers<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<LEADER>fh', ':Telescope help_tags<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<LEADER>fr', ':Telescope resume<CR>', { noremap = true })
 
