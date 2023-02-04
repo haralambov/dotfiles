6b7e86ed03796fc77f9cfdeb7ff09afe7b65bfcb
Zlatomir Haralambov <z.haralambov@gmail.com>
2023-02-04 21:16:20 +0200

    Removed unused i3 and debian-related symlinks

--- dotfile_mapper.sh
+++ dotfile_mapper.sh
@@ -11,31 +11,6 @@ fi
 ln -s "$PWD/nvim/init.lua" "$HOME/.config/nvim/init.lua"
 ln -s "$PWD/nvim/lua" "$HOME/.config/nvim/lua"
 
-# map i3 config
-if [ ! -d "$HOME/.config/i3" ]; then
-    mkdir "$HOME/.config/i3"
-else
-    rm "$HOME/.config/i3/config"
-fi
-
-ln -s "$PWD/config/i3/config" "$HOME/.config/i3/config"
-
-# map i3blocks config
-if [ ! -d "$HOME/.config/i3blocks" ]; then
-    mkdir "$HOME/.config/i3blocks"
-else
-    rm "$HOME/.config/i3blocks/config"
-fi
-
-ln -s "$PWD/config/i3blocks/config" "$HOME/.config/i3blocks/config"
-
-# map bashrc config
-if [ -f "$HOME/.bashrc" ]; then
-    rm "$HOME/.bashrc"
-fi
-
-ln -s "$PWD/.bashrc" "$HOME/.bashrc"
-
 # map zshrc config
 if [ -f "$HOME/.zshrc" ]; then
     rm "$HOME/.zshrc"
@@ -43,19 +18,44 @@ fi
 
 ln -s "$PWD/.zshrc" "$HOME/.zshrc"
 
-# maps touchpad config, that enables tap to click
-if [ -f "/etc/X11/xorg.conf.d/40-libinput.conf" ]; then
-    sudo rm "/etc/X11/xorg.conf.d/40-libinput.conf"
-fi
+## map i3 config
+#if [ ! -d "$HOME/.config/i3" ]; then
+#    mkdir "$HOME/.config/i3"
+#else
+#    rm "$HOME/.config/i3/config"
+#fi
+#
+#ln -s "$PWD/config/i3/config" "$HOME/.config/i3/config"
+#
+## map i3blocks config
+#if [ ! -d "$HOME/.config/i3blocks" ]; then
+#    mkdir "$HOME/.config/i3blocks"
+#else
+#    rm "$HOME/.config/i3blocks/config"
+#fi
+#
+#ln -s "$PWD/config/i3blocks/config" "$HOME/.config/i3blocks/config"
+#
+## map bashrc config
+#if [ -f "$HOME/.bashrc" ]; then
+#    rm "$HOME/.bashrc"
+#fi
+#
+#ln -s "$PWD/.bashrc" "$HOME/.bashrc"
 
-sudo ln -s "$PWD/config/touchpad/40-libinput.conf" "/etc/X11/xorg.conf.d/40-libinput.conf"
-
-# map compton config
-if [ ! -d "$HOME/.config/compton" ]; then
-    mkdir "$HOME/.config/compton"
-else
-    rm "$HOME/.config/compton/compton.conf"
-fi
-
-ln -s "$PWD/config/compton/compton.conf" "$HOME/.config/compton/compton.conf"
-
+## maps touchpad config, that enables tap to click
+#if [ -f "/etc/X11/xorg.conf.d/40-libinput.conf" ]; then
+#    sudo rm "/etc/X11/xorg.conf.d/40-libinput.conf"
+#fi
+#
+#sudo ln -s "$PWD/config/touchpad/40-libinput.conf" "/etc/X11/xorg.conf.d/40-libinput.conf"
+#
+## map compton config
+#if [ ! -d "$HOME/.config/compton" ]; then
+#    mkdir "$HOME/.config/compton"
+#else
+#    rm "$HOME/.config/compton/compton.conf"
+#fi
+#
+#ln -s "$PWD/config/compton/compton.conf" "$HOME/.config/compton/compton.conf"
+#
