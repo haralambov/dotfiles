d34b7e739835620a19d7e279c72596f38087d454
Zlatomir Haralambov <z.haralambov@gmail.com>
2023-03-06 20:03:23 +0200

    Removes background color to gain terminal transparency

--- nvim/lua/plugins_configs/theme.lua
+++ nvim/lua/plugins_configs/theme.lua
@@ -1,3 +1,4 @@
-vim.cmd[[colorscheme gruvbox]]
 vim.g.gruvbox_contrast_dark = 'hard'
 vim.opt.background ='dark'
+vim.cmd([[colorscheme gruvbox]])
+vim.cmd([[highlight Normal ctermbg=NONE guibg=NONE]])
