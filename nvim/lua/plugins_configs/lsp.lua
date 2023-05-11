960e4fb7d8df4cbb18226b91f36851bf1c3528f4
Zlatomir Haralambov <z.haralambov@gmail.com>
2023-05-11 17:21:06 +0300

    Changes nvim lsp diagnostics icons

--- nvim/lua/plugins_configs/lsp.lua
+++ nvim/lua/plugins_configs/lsp.lua
@@ -12,3 +12,10 @@ lsp.on_attach(function(client, buffnr)
 end)
 
 lsp.setup()
+
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
