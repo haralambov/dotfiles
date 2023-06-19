3dad9201cb8eb497e9d7ee7ad42d71ed8dda2643
Zlatomir Haralambov <z.haralambov@gmail.com>
2023-06-19 19:21:31 +0300

    Updates battery percentage script

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+ACPI_OUTPUT=$(acpi)
+STATUS=$(echo $ACPI_OUTPUT | cut -d: -f2 | cut -d, -f1 | tr -d "[:space:]")
+PERCENTAGE=$(echo $ACPI_OUTPUT | cut -d, -f2 | tr -d "[:space:]")
+PERCENTAGE_VALUE=$(echo $PERCENTAGE | cut -d% -f1)
+ICON=""
+# Discharging
+# Not charging
+# Charging
+if [ "$STATUS" = "Charging" ]; then
+    ICON=""
+elif [ "$STATUS" = "Not charging" ]; then
+    ICON=""
+elif [ "$STATUS" = "Full" ]; then
+    ICON=""
+elif [ "$PERCENTAGE_VALUE" -ge 90 ]; then
+    ICON=""
+elif [ "$PERCENTAGE_VALUE" -ge 75 ]; then
+    ICON=""
+elif [ "$PERCENTAGE_VALUE" -ge 50 ]; then
+    ICON=""
+elif [ "$PERCENTAGE_VALUE" -ge 25 ]; then
+    ICON=""
+else
+    ICON=""
+fi
 
-upower -i $(upower -e | grep -i BAT) | grep -i percentage | awk '{print $2}'
-
+echo "$ICON  $PERCENTAGE"
