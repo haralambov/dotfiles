1d12aea7f512ea27d63066e23e557273d8b4d9b1
Zlatomir Haralambov <z.haralambov@gmail.com>
2023-10-29 00:19:28 +0300

    Updates telescope tag version

--- nvim/lua/plugins_list.lua
+++ nvim/lua/plugins_list.lua
@@ -7,7 +7,7 @@ return require('packer').startup(function(use)
     use { "catppuccin/nvim", as = "catppuccin" }
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
