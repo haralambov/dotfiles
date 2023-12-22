70f18c66572ac90292263fd1502086c16a40e888
Zlatomir Haralambov <z.haralambov@gmail.com>
2023-12-23 00:55:44 +0200

    Adds nvim autocmd to format and organize imports on .go files save

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,1 +1,11 @@
-vim.cmd [[autocmd BufWritePre *.go lua vim.lsp.buf.format()]]
+function onSaveActions()
+    vim.api.nvim_command('lua vim.lsp.buf.format()')
+    vim.api.nvim_command('lua vim.lsp.buf.code_action({apply=true, context={only={"source.organizeImports"}}})')
+end
+
+vim.api.nvim_exec([[
+  augroup OnSaveCmdGroup
+    autocmd!
+    autocmd BufWritePre *.go lua onSaveActions()
+  augroup END
+]], false)
