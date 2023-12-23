c872e39d57bf5f5d551feabc4e2c0b1b6e768830
Zlatomir Haralambov <z.haralambov@gmail.com>
2023-12-23 23:35:00 +0200

    Adds nvim harpoon shortcuts

--- nvim/lua/plugins_configs/harpoon.lua
+++ nvim/lua/plugins_configs/harpoon.lua
@@ -8,3 +8,7 @@ vim.keymap.set("n", "<leader>j", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<leader>k", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<leader>l", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<leader>;", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<leader>P", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<leader>N", function() harpoon:list():next() end)
