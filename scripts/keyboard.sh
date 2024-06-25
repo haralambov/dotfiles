c36bb8734b9b778b8f5059e4a30b48068f9c7d6a
Zlatomir Haralambov <z.haralambov@gmail.com>
2024-06-25 16:52:46 +0300

    Changes i3 mod key to super and swaps alt with super

--- scripts/keyboard.sh
+++ scripts/keyboard.sh
@@ -2,5 +2,5 @@
 
 # Maps the Caps Lock to behave like Escape if pressed once,
 # and as Control if combined with something else
-setxkbmap -option; setxkbmap -option "ctrl:nocaps" -option grp:ctrls_toggle,grp_led:scroll us,bg -variant ,phonetic
+setxkbmap -option; setxkbmap -option "ctrl:nocaps" -option grp:ctrls_toggle,grp_led:scroll us,bg -variant ,phonetic -option altwin:swap_alt_win
 xcape -e 'Control_L=Escape' -t 175
