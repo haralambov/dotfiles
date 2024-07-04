92d2d3b2d81026033a0ca7cbc64ee8fc14657bed
Zlatomir Haralambov <z.haralambov@gmail.com>
2024-07-04 07:45:03 +0300

    Adds nvim go to implementations lsp shortcut

--- nvim/lua/plugins_configs/lsp.lua
+++ nvim/lua/plugins_configs/lsp.lua
@@ -5,6 +5,7 @@ lsp_zero.on_attach(function(client, bufnr)
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
