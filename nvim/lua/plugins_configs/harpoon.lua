de7999ebf83431a37a9c266fc4d6c6bb32bb3e22
Zlatomir Haralambov <z.haralambov@gmail.com>
2024-08-03 23:10:59 +0300

    Updated deprecated harpoon command

--- nvim/lua/plugins_configs/harpoon.lua
+++ nvim/lua/plugins_configs/harpoon.lua
@@ -1,7 +1,7 @@
 local harpoon = require("harpoon")
 harpoon:setup()
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<leader>j", function() harpoon:list():select(1) end)
