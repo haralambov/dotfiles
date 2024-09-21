89a867d12df5c450e71a8a6adc67c0943208f1de
Zlatomir Haralambov <z.haralambov@gmail.com>
2024-09-21 21:11:16 +0300

    Updated nvim-tree config so that it remembers tree window width between toggles

--- nvim/lua/plugins_configs/tree.lua
+++ nvim/lua/plugins_configs/tree.lua
@@ -1,13 +1,25 @@
-require("nvim-tree").setup({
-    update_focused_file = {
-        enable = true
-    },
-    view = {
-        side = "left",
-        width = 35,
-        number = true,
-        relativenumber = true
-    }
-})
+local tree_width = 35
 
-vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true })
+function ToggleNvimTree()
+    local view = require("nvim-tree.view")
+    if view.is_visible() then
+        tree_width = vim.api.nvim_win_get_width(view.get_winnr())
+        vim.cmd("NvimTreeToggle")
+    else
+        require("nvim-tree").setup({
+            update_focused_file = {
+                enable = true
+            },
+            view = {
+                side = "left",
+                width = tree_width,
+                number = true,
+                relativenumber = true
+            }
+        })
+        vim.cmd("NvimTreeToggle")
+        vim.cmd("wincmd p")
+    end
+end
+
+vim.keymap.set('n', '<C-n>', ':lua ToggleNvimTree()<CR>', { noremap = true })
