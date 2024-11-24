234e4dfa31bddae8c76e637bf46d83f3520db2e5
Zlatomir Haralambov <z.haralambov@gmail.com>
2024-11-24 22:09:40 +0200

    Added bat command theme config option

--- scripts/ff.sh
+++ scripts/ff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-selected_files=$(fzf -m --preview='bat --style=numbers --color=always {}' --bind shift-up:preview-page-up,shift-down:preview-page-down)
+selected_files=$(fzf -m --preview='bat --theme=OneHalfDark --style=numbers --color=always {}' --bind shift-up:preview-page-up,shift-down:preview-page-down)
 
 if [[ ! -z ${selected_files} ]]; then
     nvim $selected_files
