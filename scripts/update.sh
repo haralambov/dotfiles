b255cc449aab90158d8e1d671d1289e6012542e2
Zlatomir Haralambov <z.haralambov@gmail.com>
2025-09-13 19:46:31 +0300

    Updated packages update script to work with both debian and arch systems

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-if [ -f /var/lib/pacman/db.lck ]; then
-    echo "n/a"
+if [[ -f /etc/debian_version ]]; then
+    echo $(apt list --upgradeable 2>/dev/null | tail -n +2 | wc -l)
 else
-    echo $(yay -Syy > /dev/null && yay -Qu | wc -l)
+    if [ -f /var/lib/pacman/db.lck ]; then
+        echo "n/a"
+    else
+        echo $(yay -Syy > /dev/null && yay -Qu | wc -l)
+    fi
 fi
