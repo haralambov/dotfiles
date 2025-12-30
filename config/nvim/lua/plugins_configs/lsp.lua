7d38dc409d232b47c393e97ab6920c0bef27ae2f
Zlatomir Haralambov <z.haralambov@gmail.com>
2025-12-30 17:48:30 +0200

    Removed vuejs neovim lsp settings

--- config/nvim/lua/plugins_configs/lsp.lua
+++ config/nvim/lua/plugins_configs/lsp.lua
@@ -31,12 +31,6 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
     ensure_installed = {},
     handlers = {
-        function(server_name)
-            if server_name == 'vue_ls' or server_name == 'vtsls' then
-                return
-            end
-            lsp_zero.default_setup(server_name)
-        end,
         lua_ls = function()
             local lua_opts = lsp_zero.nvim_lua_ls()
             vim.lsp.config('lua_ls',lua_opts)
@@ -44,50 +38,6 @@ require('mason-lspconfig').setup({
         end,
     },
 })
-
-local capabilities = lsp_zero.get_capabilities
-and lsp_zero.get_capabilities()
-or nil
-
-local vue_language_server_path =
-vim.fn.stdpath('data')
-.. '/mason/packages/vue-language-server/node_modules/@vue/language-server'
-
-local vue_plugin = {
-    name = '@vue/typescript-plugin',
-    location = vue_language_server_path,
-    languages = { 'vue' },
-    configNamespace = 'typescript',
-}
-
-vim.lsp.config('vtsls', {
-    on_attach = lsp_zero.on_attach,
-    capabilities = capabilities,
-    filetypes = {
-        'javascript',
-        'javascriptreact',
-        'javascript.jsx',
-        'typescript',
-        'typescriptreact',
-        'typescript.tsx',
-        'vue',
-    },
-    settings = {
-        vtsls = {
-            tsserver = {
-                globalPlugins = { vue_plugin },
-            },
-        },
-    },
-})
-vim.lsp.enable('vtsls')
-
-vim.lsp.config('vue_ls',{
-    on_attach = lsp_zero.on_attach,
-    capabilities = capabilities,
-    filetypes = { 'vue' },
-})
-vim.lsp.enable('vue_ls')
 
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
